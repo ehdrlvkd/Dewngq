923a5d57204add79182324a7bc0299e2f20c85f0
이동규 <ehdrlvkd@naver.com>
2022-03-03 22:14:06 +0900

    slack bot test

--- APITEST.py
+++ APITEST.py
@@ -1,22 +1,34 @@
+from urllib import response
 import requests
+import json
 import appkey
 
-
-appkey = appkey.appkey
-
-start_date = '2021-01-01 00:00:00'
-end_date = '2021-12-31 00:00:00'
+# start_date = '2021-01-01 00:00:00'
+# end_date = '2021-12-31 00:00:00'
 
 
-headers={'Authorization':appkey}
-#AppURL = f'https://api.nexon.co.kr/kart/v1.0/users/nickname/{nickname}'
-matchURL = f'https://api.nexon.co.kr/kart/v1.0/matches/all?start_date={start_date}&end_date={end_date}'
+# headers={'Authorization':appkey}
+# #AppURL = f'https://api.nexon.co.kr/kart/v1.0/users/nickname/{nickname}'
+# matchURL = f'https://api.nexon.co.kr/kart/v1.0/matches/all?start_date={start_date}&end_date={end_date}'
 
-##req=requests.get(AppURL,headers=headers)
-req=requests.get(matchURL,headers=headers)
+# ##req=requests.get(AppURL,headers=headers)
+# req=requests.get(matchURL,headers=headers)
 
-if req.status_code == 200:
-    print("정상출력")
-    print(req.json())
-else:
-    print(req.status_code)
+# if req.status_code == 200:
+#     print("정상출력")
+#     print(req.json())
+# else:
+#     print(req.status_code)
+
+slack_token = appkey.slack_token
+
+def post_message (token, channel, text):
+
+    response = requests.post("https://slack.com/api/chat.postMessage",
+    headers = {"Authorization" : "Bearer " + token},
+    data = {"channel" : channel , "text" : text}
+    )
+
+    print(response)
+
+post_message(slack_token,'#portfolio','success message post')
